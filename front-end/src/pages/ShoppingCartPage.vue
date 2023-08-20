94914e3cbf4d27c4af74f93aec7e29b1f0a99ef7
Naseh <75029758+NasehR@users.noreply.github.com>
2023-08-20 18:19:33 +0800

    Challenge completed.

--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -1,18 +1,7 @@
 <template>
     <h1>Shopping cart</h1>
     <div v-if="cartItems.length > 0">
-        <div 
-            class="product-container"
-            v-for="product in cartItems"
-            :key="product.id"
-        >
-            <img class="product-image" :src="product.imageName" />
-            <div class="detail-wrap">
-                <h3>{{ product.name }}</h3>
-                <p>{{ product.price }}</p>
-            </div>
-            <button class="remove-button">Remove from cart</button>
-        </div>
+        <ShoppingCartList :products="cartItems" />
         <button class="checkout-button">Proceed to checkout</button>
     </div>
     <div v-if="cartItems.length === 0">
@@ -22,9 +11,13 @@
 
 <script>
 import { cartItems } from '../temp-data';
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
 
 export default {
     name: "ShoppingCartPage",
+    components: {
+        ShoppingCartList,
+    },
     data() {
         return {
             cartItems,
